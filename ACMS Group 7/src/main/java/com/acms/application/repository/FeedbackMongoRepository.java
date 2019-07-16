dcef100cba55ca3a87145771c8265dde3e488de2
Ankita Rai <43443708+Ankita2518@users.noreply.github.com>
2019-07-16 22:28:33 +0530

    Delete FeedbackMongoRepository.java

--- ACMS Group 7/src/main/java/com/acms/application/repository/FeedbackMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acms.application.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.acms.application.model.Feedback;
-
-public interface FeedbackMongoRepository extends CrudRepository<Feedback, String>{}
